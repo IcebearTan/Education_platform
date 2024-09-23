5a9853f61f97f20264bc6aa4cecef6dd0f2b6064
Jerry <jerrycaocao@126.com>
2024-09-23 22:33:11 +0800

    0.1.1 完善了登录注册功能

--- BME_frontend/src/api.js
+++ BME_frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://110.41.177.107:8000';
+// const API_URL = 'http://110.41.177.107:8000';
+const API_URL = 'http://139.159.157.5:5000';
 // const API_URL = 'http://localhost:8000';
 
 
