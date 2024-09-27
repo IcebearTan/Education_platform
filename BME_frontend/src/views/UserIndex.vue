eeba264472199c46a5e0b95254ff379632ca73bb
Jerry <xiaojieanchor@gmail.com>
2024-09-27 20:51:10 +0800

    0.2.3 将菜单模块封装进独立的组件方便调用，修复了一些bug

--- BME_frontend/src/views/UserIndex.vue
+++ BME_frontend/src/views/UserIndex.vue
@@ -1,18 +1,30 @@
 <script>
 import UserindexComponent from "../components/UserindexComponent.vue";
-
-import { ref } from 'vue'
-
-const activeIndex = ref('1')
-const handleSelect = () => {
-    console.log()
-}
+import MenuComponent from "../components/MenuComponent.vue";
 
 export default {
+    // data() {
+    //     return {
+    //         activeIndex: "/"
+    //     }
+    // },
+
+    // methods: {
+    //   handleSelect(key, keyPath) {
+    //     console.log(key, keyPath);
+    //   }
+    // },
+
+    // created() {
+    //     this.activeIndex = this.$route.path
+    // },
+
     name: 'UserIndex',
     components: {
-        UserindexComponent
+        UserindexComponent,
+        MenuComponent
     },
+    
 };
 
 
@@ -22,44 +34,41 @@ export default {
     <div class="common-layout">
         <el-container>
             <el-header>
-                <el-menu :default-active="this.$router.path" class="el-menu-demo" mode="horizontal" :ellipsis="false"
+                <!-- <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :ellipsis="false"
                     @select="handleSelect" router>
                     <el-menu-item index="/">
                         <img style="width: 50px" src="../assets/vue.svg" />
                     </el-menu-item>
 
                     <el-sub-menu index="2">
-                        <template #title>Workspace</template>
-                        <el-menu-item index="2-1">item one</el-menu-item>
-                        <el-menu-item index="2-2">item two</el-menu-item>
-                        <el-menu-item index="2-3">item three</el-menu-item>
+                        <template #title>文章学习</template>
+                        <el-menu-item index="2-1">分流前</el-menu-item>
                         <el-sub-menu index="2-4">
-                            <template #title>item four</template>
-                            <el-menu-item index="2-4-1">item one</el-menu-item>
-                            <el-menu-item index="2-4-2">item two</el-menu-item>
-                            <el-menu-item index="2-4-3">item three</el-menu-item>
+                            <template #title>分流后</template>
+                            <el-menu-item index="2-4-1">硬件组</el-menu-item>
+                            <el-menu-item index="2-4-2">软件组</el-menu-item>
+                            <el-menu-item index="2-4-3">先进制造组</el-menu-item>
                         </el-sub-menu>
                     </el-sub-menu>
                     <el-sub-menu index="3" :popper-offset="8">
-                        <template #title>Override Popper Offset</template>
-                        <el-menu-item index="3-1">item one</el-menu-item>
-                        <el-menu-item index="3-2">item two</el-menu-item>
-                        <el-menu-item index="3-3">item three</el-menu-item>
+                        <template #title>学习进度考核</template>
+                        <el-menu-item index="3-1">分流前</el-menu-item>
                         <el-sub-menu index="3-4" :popper-offset="20">
-                            <template #title>override child</template>
-                            <el-menu-item index="3-4-1">item one</el-menu-item>
-                            <el-menu-item index="3-4-2">item two</el-menu-item>
-                            <el-menu-item index="3-4-3">item three</el-menu-item>
+                            <template #title>分流后</template>
+                            <el-menu-item index="3-4-1">硬件组</el-menu-item>
+                            <el-menu-item index="3-4-2">软件组</el-menu-item>
+                            <el-menu-item index="3-4-3">先进制造组</el-menu-item>
                         </el-sub-menu>
                     </el-sub-menu>
                     <el-menu-item index="/login">登录/注册</el-menu-item>
                     <el-menu-item index="/user">个人中心</el-menu-item>
-                </el-menu>
+                </el-menu> -->
+                <MenuComponent/>
             </el-header>
             <el-main>
                 <UserindexComponent />
             </el-main>
-            <el-footer>Footer</el-footer>
+            <el-footer>知行合一</el-footer>
         </el-container>
     </div>
 </template>
