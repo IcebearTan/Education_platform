2904e2052a3577fc697752e7940ac86e061c956e
IcebearTan <614942807@qq.com>
2024-11-24 16:51:21 +0800

    202411241651 添加了左侧导航栏和用户管理页面

--- BME_backend/src/views/HomeView.vue
+++ BME_backend/src/views/HomeView.vue
@@ -11,12 +11,14 @@ export default {
 
   data() {
     return {
+      activeIndex: '/',
+
       store: useStore(),
       router: useRouter(),
     };
   },
 
-  created() {
+  async created() {
     api({
       url: "/user/user_index",
       method: "get",
@@ -37,6 +39,14 @@ export default {
       }
     }
     )
+
+    if (this.$route.path === '/') {
+      this.activeIndex = '/user-manage'
+    }
+    else {
+      this.activeIndex = this.$route.path
+    }
+    console.log(this.activeIndex)
   }
 };
 </script>
@@ -54,8 +64,111 @@ import { useStore } from 'vuex'
 
 <template>
   <div>
-    <h1>主页</h1>
+    <div class="common-layout">
+      <el-container>
+        <el-aside width="200px" position="relative">
+          <!-- <h5 class="title">训练营<br>后台管理系统</h5> -->
+          <el-menu active-text-color="#ffd04b" background-color="#545c64" class="el-menu-vertical-demo"
+            :default-active=this.activeIndex text-color="#fff" @open="handleOpen" @close="handleClose">
+            <div class="title">训练营<br>后台管理系统</div>
+            <el-sub-menu index="1">
+              <template #title>
+                <el-icon>
+                  <location />
+                </el-icon>
+                <span>文章管理</span>
+              </template>
+              <el-menu-item index="/user-manage" @click="router.push('/user-manage')">管理文章</el-menu-item>
+            </el-sub-menu>
+            <el-sub-menu index="2">
+              <template #title>
+                <el-icon>
+                  <location />
+                </el-icon>
+                <span>文章管理</span>
+              </template>
+              <el-menu-item index="2-1">编辑文章</el-menu-item>
+              <el-menu-item index="2-2">管理文章</el-menu-item>
+            </el-sub-menu>
+            <el-sub-menu index="3">
+              <template #title>
+                <el-icon>
+                  <location />
+                </el-icon>
+                <span>文章管理</span>
+              </template>
+              <el-menu-item index="3-1">编辑文章</el-menu-item>
+              <el-menu-item index="3-2">管理文章</el-menu-item>
+            </el-sub-menu>
+            <el-sub-menu index="4">
+              <template #title>
+                <el-icon>
+                  <location />
+                </el-icon>
+                <span>文章管理</span>
+              </template>
+              <el-menu-item index="4-1">编辑文章</el-menu-item>
+              <el-menu-item index="4-2">管理文章</el-menu-item>
+            </el-sub-menu>
+            <el-sub-menu index="5">
+              <template #title>
+                <el-icon>
+                  <location />
+                </el-icon>
+                <span>文章管理</span>
+              </template>
+              <el-menu-item index="5-1">编辑文章</el-menu-item>
+              <el-menu-item index="5-2">管理文章</el-menu-item>
+            </el-sub-menu>
+            <el-sub-menu index="6">
+              <template #title>
+                <el-icon>
+                  <location />
+                </el-icon>
+                <span>文章管理</span>
+              </template>
+              <el-menu-item index="6-1">编辑文章</el-menu-item>
+              <el-menu-item index="6-2">管理文章</el-menu-item>
+            </el-sub-menu>
+
+            <el-menu-item index="7" style="position: absolute; bottom: 0;">
+              <el-icon>
+                <setting />
+              </el-icon>
+              <span>系统设置</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <router-view />
+        </el-main>
+      </el-container>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.common-layout {
+  width: 100%;
+  min-height: 100%;
+  /* background-color: #f5f5f5; */
+}
+
+.el-menu-vertical-demo {
+  min-height: 100vh;
+}
+
+.title {
+  text-align: center;
+  color: #fff;
+  margin: 0px;
+
+  height: 60px;
+  padding: 10px;
+
+  font-size: large;
+  font-weight: 900;
+
+  background-color: #545c64;
+}
+</style>
