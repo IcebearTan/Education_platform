510a1f7c3089eca34aa3021c8f5e9385e3d9c285
IcebearTan <614942807@qq.com>
2025-02-21 16:18:35 +0800

    202502211618 添加了勋章的静态素材，并修改了勋章墙静态页面

--- BME_frontend/src/components/MedalWallComponent.vue
+++ BME_frontend/src/components/MedalWallComponent.vue
@@ -1,165 +1,35 @@
-<script>
-export default {
-  name: 'MedalWall',
-  data() {
-    return {
-      currentCategory: '',
-      medals: [
-        {
-          id: 1,
-          name: 'STM32',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/STM32.png', //图片存在public里面
-          category: '硬件组',
-        },
-        {
-          id: 2,
-          name: 'GD32',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/GD32.png', 
-          category: '硬件组',
-        },
-        {
-          id: 3,
-          name: '微机原理',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/MicroTech.png', 
-          category: '硬件组',
-        },
-        {
-          id: 4,
-          name: 'μC/OS-Ⅲ原理与应用开发',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/uCOS3Dev.png',
-          category: '硬件组',
-        },
-        {
-          id: 5,
-          name: 'Altium Designer',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/AD.png',
-          category: '硬件组',
-        },
-        {
-          id: 6,
-          name: '立创EDA',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/JLC-EDA.png',
-          category: '硬件组',
-        },
-        {
-          id: 7,
-          name: 'Multisim',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/Multisim.png',
-          category: '硬件组',
-        },
-        {
-          id: 8,
-          name: 'MATLAB',
-          date: '获得日期：2025.1.1',
-          icon:'/medals/MATLAB.png',
-          category: '硬件组',
-        },
-        {
-          id: 9,
-          name: '焊接',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/WeldTech.png',
-          category: '硬件组',
-        },
-        {
-          id: 10,
-          name: '体温',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/TempModule.png',
-          category: '硬件组',
-        },
-        {
-          id: 11,
-          name: '心电',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/ECGModule.png',
-          category: '硬件组',
-        },
-        {
-          id: 12,
-          name: '血氧',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/SpO2Module.png',
-          category: '硬件组',
-        },
-        {
-          id: 13,
-          name: 'VTK手术机器人',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/Default.png',
-          category: '软件组',
-        },
-        {
-          id: 14,
-          name: 'C语言程序设计',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/CLang.png',
-          category: '软件组',
-        },
-        {
-          id: 15,
-          name: 'C++程序设计',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/Cpp.png',
-          category: '软件组',
-        },
-        {
-          id: 16,
-          name: 'Qt程序设计',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/QT.png',
-          category: '软件组',
-        },
-        {
-          id: 17,
-          name: '骨干-蓝',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/EliteBlue.png',
-          category: '特殊勋章',
-        },
-        {
-          id: 18,
-          name: '骨干-黄',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/EliteYellow.png',
-          category: '特殊勋章',
-        },
-        {
-          id: 19,
-          name: '开拓先锋',
-          date: '获得日期：2025.1.1',
-          icon: '/medals/PioneerX.png',
-          category: '特殊勋章',
-        },
-      ],
-    };
-  },
-  methods: {
-    medalClass(name) {
-      if (name === 'VTK手术机器人') {
-        return 'medal-icon';
-      } else {
-        return 'medal-icon';
-      }
-    }
-  },
-  computed:{
-    filteredMedals(){
-      if(this.currentCategory===''){
-        return this.medals;
-      }else{
-        return this.medals.filter(medal=>medal.category===this.currentCategory);
-      }
-    },
-  },
+<script setup>
+import { ref, computed } from 'vue';
+import api from '../api';
+
+const currentCategory = ref('');
+
+const medalClass = (name) => {
+  if (name === 'VTK手术机器人') {
+    return 'medal-icon';
+  } else {
+    return 'medal-icon';
+  }
 };
+
+const filteredMedals = computed(() => {
+  if (currentCategory.value === '') {
+    return medals.value;
+  } else {
+    return medals.value.filter(medal => medal.category === currentCategory.value);
+  }
+});
+
+const medals = ref([])
+
+const fetchMedals = async () => {
+  try {
+    const res = await api('https://api.example.com/medals');
+    console.log(res);
+  } catch (error) {
+    console.error('Error fetching medals:', error);
+  }
+}
 </script>
 
 <template>
@@ -329,20 +199,34 @@ body,#app, .app-main {
   background: #fff;
   box-shadow: 0px 0px 8px rgba(203, 213, 224, 0.838);
   height: 200px;
+
+  transition: 0.3s;
+  cursor: pointer;
+
+  overflow: hidden;
+  object-position: center;
+}
+.medal:hover {
+  transform: translateY(-10px);
 }
 
 .medal-icon {
-  width: 180px;
-  height: 180px;
+  width: 145px;
+  height: 145px;
   /* margin-bottom: 10px; */
-  border-radius: 8px;
+  border-radius: 50%;
 
   position: relative;
-  top: -20px;
+  /* top: -20px; */
+  /* left: 3px; */
 
-  opacity: 0.1;
-  filter: grayscale(90%);
+  /* opacity: 0.1;
+  filter: grayscale(60%); */
+  
+  box-shadow: 0px 0px 20px #bbb, 0px 0px 20px #bbb inset;
 
+  object-fit: cover;
+  
 }
 
 .medal-icon-old {
@@ -354,7 +238,7 @@ body,#app, .app-main {
 
 .medal-info {
   position: relative;
-  top: -35px;
+  /* top: -35px; */
 }
 
 .medal-info h3 {
@@ -372,4 +256,234 @@ body,#app, .app-main {
 </style>
 
 
+{
+  "Medal_Name": "STM32",
+  "Medal_Name_CN": "STM32",
+  "Medal_Tag": "硬件组"
+},
+{
+  "Medal_Name": "GD32",
+  "Medal_Name_CN": "GD32",
+  "Medal_Tag": "硬件组"
+},
+{
+  "Medal_Name": "MicroTech",
+  "Medal_Name_CN": "微机原理",
+  "Medal_Tag": "硬件组"
+},
+{
+  "Medal_Name": "uCOS3Dev",
+  "Medal_Name_CN": "μC/OS-Ⅲ原理与应用开发",
+  "Medal_Tag": "硬件组"
+},
+{
+  "Medal_Name": "AD",
+  "Medal_Name_CN": "Altium Designer",
+  "Medal_Tag": "硬件组"
+},
+{
+  "Medal_Name": "JLC-EDA",
+  "Medal_Name_CN": "立创EDA",
+  "Medal_Tag": "硬件组"
+},
+{
+  "Medal_Name": "Multisim",
+  "Medal_Name_CN": "Multisim",
+  "Medal_Tag": "硬件组"
+},
+{
+  "Medal_Name": "MATLAB",
+  "Medal_Name_CN": "MATLAB",
+  "Medal_Tag": "硬件组"
+},
+{
+  "Medal_Name": "WeldTech",
+  "Medal_Name_CN": "焊接",
+  "Medal_Tag": "硬件组"
+},
+{
+  "Medal_Name": "TempModule",
+  "Medal_Name_CN": "体温",
+  "Medal_Tag": "硬件组"
+},
+{
+  "Medal_Name": "ECGModule",
+  "Medal_Name_CN": "心电",
+  "Medal_Tag": "硬件组"
+},
+{
+  "Medal_Name": "SpO2Module",
+  "Medal_Name_CN": "血氧",
+  "Medal_Tag": "硬件组"
+},
+{
+  "Medal_Name": "VTK",
+  "Medal_Name_CN": "VTK手术机器人",
+  "Medal_Tag": "软件组"
+},
+{
+  "Medal_Name": "CLang",
+  "Medal_Name_CN": "C语言程序设计",
+  "Medal_Tag": "软件组"
+},
+{
+  "Medal_Name": "Cpp",
+  "Medal_Name_CN": "C++程序设计",
+  "Medal_Tag": "软件组"
+},
+{
+  "Medal_Name": "QT",
+  "Medal_Name_CN": "Qt程序设计",
+  "Medal_Tag": "软件组"
+},
+{
+  "Medal_Name": "EliteBlue",
+  "Medal_Name_CN": "骨干-蓝",
+  "Medal_Tag": "特殊勋章"
+},
+{
+  "Medal_Name": "EliteYellow",
+  "Medal_Name_CN": "骨干-黄",
+  "Medal_Tag": "特殊勋章"
+},
+{
+  "Medal_Name": "PioneerX",
+  "Medal_Name_CN": "开拓先锋",
+  "Medal_Tag": "特殊勋章"
+}
 
+medals: [
+        {
+          id: 1,
+          name: 'STM32',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/STM32.png', //图片存在public里面
+          category: '硬件组',
+        },
+        {
+          id: 2,
+          name: 'GD32',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/GD32.png', 
+          category: '硬件组',
+        },
+        {
+          id: 3,
+          name: '微机原理',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/MicroTech.png', 
+          category: '硬件组',
+        },
+        {
+          id: 4,
+          name: 'μC/OS-Ⅲ原理与应用开发',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/uCOS3Dev.png',
+          category: '硬件组',
+        },
+        {
+          id: 5,
+          name: 'Altium Designer',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/AD.png',
+          category: '硬件组',
+        },
+        {
+          id: 6,
+          name: '立创EDA',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/JLC-EDA.png',
+          category: '硬件组',
+        },
+        {
+          id: 7,
+          name: 'Multisim',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/Multisim.png',
+          category: '硬件组',
+        },
+        {
+          id: 8,
+          name: 'MATLAB',
+          date: '获得日期：2025.1.1',
+          icon:'/medals/MATLAB.png',
+          category: '硬件组',
+        },
+        {
+          id: 9,
+          name: '焊接',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/WeldTech.png',
+          category: '硬件组',
+        },
+        {
+          id: 10,
+          name: '体温',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/TempModule.png',
+          category: '硬件组',
+        },
+        {
+          id: 11,
+          name: '心电',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/ECGModule.png',
+          category: '硬件组',
+        },
+        {
+          id: 12,
+          name: '血氧',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/SpO2Module.png',
+          category: '硬件组',
+        },
+        {
+          id: 13,
+          name: 'VTK手术机器人',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/VTK.png',
+          category: '软件组',
+        },
+        {
+          id: 14,
+          name: 'C语言程序设计',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/CLang.png',
+          category: '软件组',
+        },
+        {
+          id: 15,
+          name: 'C++程序设计',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/Cpp.png',
+          category: '软件组',
+        },
+        {
+          id: 16,
+          name: 'Qt程序设计',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/QT.png',
+          category: '软件组',
+        },
+        {
+          id: 17,
+          name: '骨干-蓝',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/EliteBlue.png',
+          category: '特殊勋章',
+        },
+        {
+          id: 18,
+          name: '骨干-黄',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/EliteYellow.png',
+          category: '特殊勋章',
+        },
+        {
+          id: 19,
+          name: '开拓先锋',
+          date: '获得日期：2025.1.1',
+          icon: '/medals/PioneerX.png',
+          category: '特殊勋章',
+        },
+      ],
