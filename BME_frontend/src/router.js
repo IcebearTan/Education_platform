b089b05ad98c45e56382f9b962e50dd0f8c3c92e
Jerry <jerrycaocao@126.com>
2025-04-18 19:45:13 +0800

    修复bug

--- BME_frontend/src/router.js
+++ BME_frontend/src/router.js
@@ -2,8 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import UserindexComponent from './components/user/UserindexComponent.vue';
 import AdminindexComponent from "./components/user/AdminindexComponent.vue";
-import UserindexComponent from './components/user/UserindexComponent.vue';
-import AdminindexComponent from "./components/user/AdminindexComponent.vue";
 
 import HomePage from './components/HomePage.vue';
 import StudyDetailsComponent from './components/StudyDetailsComponent.vue'
@@ -13,20 +11,14 @@ import ExamListComponent from './components/ExamListComponent.vue'
 import HomeView from './views/HomeView.vue'
 import LoginView from './views/auth/LoginView.vue'
 import RegisterView from './views/auth/RegisterView.vue'
-import LoginView from './views/auth/LoginView.vue'
-import RegisterView from './views/auth/RegisterView.vue'
 import ArticleView from './views/ArticleView.vue';
 import StudyView from './views/StudyView.vue'
 import ExamView from './views/ExamView.vue'
 import UserIndex from './views/user/UserIndex.vue';
 import UserCenter from './views/user/UserCenter.vue';
-import UserIndex from './views/user/UserIndex.vue';
-import UserCenter from './views/user/UserCenter.vue';
 import UserInfoComponent from './components/UserInfoComponent.vue';
 
 import AboutUsView from './views/AboutUsView.vue'
-import FindPasswordView from './views/auth/FindPasswordView.vue'
-import MedalWallComponent from './components/medal/MedalWallComponent.vue';
 import FindPasswordView from './views/auth/FindPasswordView.vue'
 import MedalWallComponent from './components/medal/MedalWallComponent.vue';
 
@@ -180,4 +172,23 @@ const router = createRouter({
     ]
 })
 
+// router.beforeEach((to, from, next) => {
+//     // 检测是否为移动设备
+//     const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    
+//     // 如果是移动设备且访问的是桌面首页
+//     if (to.path === '/' && isMobile) {
+//         next('/mobile')
+//     } 
+//     // 如果是桌面设备且访问的是移动首页
+//     else if (to.path === '/mobile' && !isMobile) {
+//         next('/')
+//     } 
+//     // 其他情况正常跳转
+//     else {
+//         next()
+//     }
+// })
+
 export default router
+
